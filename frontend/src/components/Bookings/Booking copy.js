7b793a35fd22c7de4fa406c2b9f4a7037412b041
Shakshi <shakshi.richhariya@sjsu.edu>
2023-11-25 03:37:31 -0800

    Implement Booking page in frontend

--- frontend/src/components/Bookings/Booking copy.js
+++ frontend/src/components/Bookings/Booking copy.js
@@ -1,13 +1,12 @@
-import { Button, FormLabel, TextField, Typography } from "@mui/material";
+import { Button, FormLabel, TextField, Typography, Select, MenuItem } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieDetails, newBooking } from "../../api-helpers/api-helpers";
+import { getMovieDetails, newBooking, getAllTheaters } from "../../api-helpers/api-helpers";
 
 const Booking = () => {
-  console.log("in booking");
   const [movie, setMovie] = useState();
-  const [inputs, setInputs] = useState({ seatNumber: "", date: "" });
+  const [inputs, setInputs] = useState({ seatNumber: "", date: "", theater: "" });
   const id = useParams().id;
   console.log(id);
 
@@ -15,13 +14,31 @@ const Booking = () => {
     getMovieDetails(id)
       .then((res) => setMovie(res.movie))
       .catch((err) => console.log(err));
+
+    getAllTheaters()
+      .then((res) => setTheaters(res.theaters))
+      .catch((err) => console.log(err));
   }, [id]);
+
+  useEffect(() => {
+    if (theaterCapacity) {
+      const newSeats = Array.from({ length: theaterCapacity }, (_, i) => ({
+        id: i + 1,
+        status: 'available' // could be 'available', 'booked', or 'selected'
+      }));
+      setSeats(newSeats);
+    }
+  }, [theaterCapacity]);
+
+  
   const handleChange = (e) => {
     setInputs((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
+  const [theaters, setTheaters] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs);
@@ -59,10 +76,6 @@ const Booking = () => {
               <Box width={"80%"} marginTop={3} padding={2}>
                 <Typography paddingTop={2}>{movie.description}</Typography>
                 <Typography fontWeight={"bold"} marginTop={1}>
-                  Starrer:
-                  {movie.actors.map((actor) => " " + actor + " ")}
-                </Typography>
-                <Typography fontWeight={"bold"} marginTop={1}>
                   Release Date: {new Date(movie.date).toDateString()}
                 </Typography>
               </Box>
@@ -75,15 +88,19 @@ const Booking = () => {
                   display="flex"
                   flexDirection={"column"}
                 >
-                  <FormLabel>Seat Number</FormLabel>
-                  <TextField
-                    name="seatNumber"
-                    value={inputs.seatNumber}
+                  <FormLabel>Theater</FormLabel>
+                  <Select
+                    name="theater"
+                    value={inputs.theater}
                     onChange={handleChange}
-                    type={"number"}
-                    margin="normal"
-                    variant="standard"
-                  />
+                    displayEmpty
+                  >
+                    <MenuItem value="">
+                    </MenuItem>
+                    {theaters.map((theater, index) => (
+                      <MenuItem key={index} value={theater._id}>{theater.theaterName}</MenuItem>
+                    ))}
+                  </Select>
                   <FormLabel>Booking Date</FormLabel>
                   <TextField
                     name="date"
@@ -92,6 +109,33 @@ const Booking = () => {
                     variant="standard"
                     value={inputs.date}
                     onChange={handleChange}
+                  />
+
+                  <Box
+                    sx={{
+                      height: '50px', // Adjust the height as needed
+                      backgroundColor: 'darkgray', // Screen color
+                      borderRadius: '50px / 10px', // This creates the curved effect
+                      textAlign: 'center',
+                      color: 'white',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      marginY: 2 // Adds vertical space around the screen
+                    }}
+                  >
+                    <Typography variant="h6">Screen</Typography>
+                  </Box>
+
+
+                  <FormLabel>Seat Number</FormLabel>
+                  <TextField
+                    name="seatNumber"
+                    value={inputs.seatNumber}
+                    onChange={handleChange}
+                    type={"number"}
+                    margin="normal"
+                    variant="standard"
                   />
                   <Button type="submit" sx={{ mt: 3 }}>
                     Book Now
--- frontend/src/components/Bookings/Booking.js
+++ frontend/src/components/Bookings/Booking.js
@@ -1,30 +1,69 @@
-import { Button, FormLabel, TextField, Typography } from "@mui/material";
+import { Button, FormLabel, TextField, Typography, Select, MenuItem, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieDetails, newBooking } from "../../api-helpers/api-helpers";
+import { getMovieDetails, newBooking, getAllTheaters } from "../../api-helpers/api-helpers";
 
 const Booking = () => {
   const [movie, setMovie] = useState();
-  const [inputs, setInputs] = useState({ seatNumber: "", date: "" });
+  const [inputs, setInputs] = useState({ seatNumber: "", date: "", theater: "" });
   const id = useParams().id;
   console.log(id);
+  const [selectedTheater, setSelectedTheater] = useState(null);
 
   useEffect(() => {
     getMovieDetails(id)
       .then((res) => setMovie(res.movie))
       .catch((err) => console.log(err));
+
+    getAllTheaters()
+      .then((res) => setTheaters(res.theaters))
+      .catch((err) => console.log(err));
   }, [id]);
+
+  const [seats, setSeats] = useState([]);
+
+  useEffect(() => {
+    if (selectedTheater && selectedTheater.capacity) {
+      const newSeats = Array.from({ length: selectedTheater.capacity }, (_, index) => ({
+        id: index + 1,
+        status: 'available' // This could also be 'booked' if you have that info
+      }));
+      setSeats(newSeats);
+    }
+  }, [selectedTheater]);
+  
+  const handleSeatClick = (seatId) => {
+    const selectedSeatsCount = seats.filter(seat => seat.status === 'selected').length;
+    const isCurrentlySelected = seats.find(seat => seat.id === seatId).status === 'selected';
+  
+    if (selectedSeatsCount < 8 || isCurrentlySelected) {
+      // Toggle seat selection
+      const updatedSeats = seats.map(seat => seat.id === seatId
+        ? { ...seat, status: seat.status === 'selected' ? 'available' : 'selected' }
+        : seat);
+      setSeats(updatedSeats);
+    } else {
+      // Optionally alert the user that the maximum number of seats has been reached
+      alert("You can select up to 8 seats.");
+    }
+  };
+  
+  
   const handleChange = (e) => {
     setInputs((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
+    setSelectedTheater(e.target.value);
   };
+  const [theaters, setTheaters] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs);
-    newBooking({ ...inputs, movie: movie._id })
+    const selectedSeatIds = seats.filter(seat => seat.status === 'selected').map(seat => seat.id);
+    newBooking({ ...inputs, movie: movie._id, seats: selectedSeatIds })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
@@ -70,15 +109,19 @@ const Booking = () => {
                   display="flex"
                   flexDirection={"column"}
                 >
-                  <FormLabel>Seat Number</FormLabel>
-                  <TextField
-                    name="seatNumber"
-                    value={inputs.seatNumber}
+                  <FormLabel>Theater</FormLabel>
+                  <Select
+                    name="theater"
+                    value={inputs.theater}
                     onChange={handleChange}
-                    type={"number"}
-                    margin="normal"
-                    variant="standard"
-                  />
+                    displayEmpty
+                  >
+                    <MenuItem value="">
+                    </MenuItem>
+                    {theaters.map((theater, index) => (
+                      <MenuItem key={index} value={theater}>{theater.theaterName}</MenuItem>
+                    ))}
+                  </Select>
                   <FormLabel>Booking Date</FormLabel>
                   <TextField
                     name="date"
@@ -88,6 +131,38 @@ const Booking = () => {
                     value={inputs.date}
                     onChange={handleChange}
                   />
+                  <Box marginBottom={5}></Box>
+                  <FormLabel>Seat Selection</FormLabel>
+                  <Box
+                    sx={{
+                      height: '50px', // Adjust the height as needed
+                      backgroundColor: 'darkgray', // Screen color
+                      borderRadius: '50px / 10px', // This creates the curved effect
+                      textAlign: 'center',
+                      color: 'white',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      marginY: 2 // Adds vertical space around the screen
+                    }}
+                  >
+                    <Typography variant="h6">Screen</Typography>
+                  </Box>
+                  <Box>
+                    <Grid container spacing={1}>
+                      {seats.map(seat => (
+                        <Grid item key={seat.id}>
+                          <Button
+                            variant={seat.status === 'selected' ? 'contained' : 'outlined'}
+                            onClick={() => handleSeatClick(seat.id)}
+                            disabled={seat.status === 'booked'}
+                          >
+                            {seat.id}
+                          </Button>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Box>
                   <Button type="submit" sx={{ mt: 3 }}>
                     Book Now
                   </Button>
